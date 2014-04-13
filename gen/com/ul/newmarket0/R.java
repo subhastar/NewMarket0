8d259a747b1c356a44a2e2aa814c3402154db8cb
Subha Gollakota <subha.gollakota@gmail.com>
2014-04-13 11:45:31 -0700

    add basic and awful camera functionality.

--- gen/com/ul/newmarket0/R.java
+++ gen/com/ul/newmarket0/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_add=0x7f080006;
-        public static final int action_settings=0x7f080005;
-        public static final int btnSave=0x7f080002;
+        public static final int action_add=0x7f080008;
+        public static final int action_settings=0x7f080007;
+        public static final int btnSave=0x7f080004;
+        public static final int button1=0x7f080003;
         public static final int etShirtName=0x7f080000;
-        public static final int lvShirts=0x7f080003;
+        public static final int ivShirtPic=0x7f080002;
+        public static final int lvShirts=0x7f080005;
         public static final int tvName=0x7f080001;
-        public static final int tvShirtName=0x7f080004;
+        public static final int tvShirtName=0x7f080006;
     }
     public static final class layout {
         public static final int activity_shirt=0x7f030000;
@@ -49,6 +51,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int save=0x7f050007;
         public static final int shirtName=0x7f050005;
+        public static final int take_pic=0x7f050008;
         public static final int title_activity_shirt=0x7f050004;
     }
     public static final class style {
--- src/com/ul/newmarket0/ShirtActivity.java
+++ src/com/ul/newmarket0/ShirtActivity.java
@@ -1,16 +1,26 @@
 package com.ul.newmarket0;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.ul.newmarket0.model.Shirt;
 
 public class ShirtActivity extends Activity {
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 5;
+
 	private EditText etShirtName;
+	private ImageView ivShirtPic;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,7 @@ public class ShirtActivity extends Activity {
 		setContentView(R.layout.activity_shirt);
 		
 		etShirtName = (EditText) findViewById(R.id.etShirtName);
+		ivShirtPic = (ImageView) findViewById(R.id.ivShirtPic);
 	}
 
 	@Override
@@ -25,6 +36,38 @@ public class ShirtActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.shirt, menu);
 		return true;
+	}
+	
+	public Uri getPhotoFileUri(String fileName) {
+		File mediaStorageDir = new File(
+				Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"MyCameraApp");
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+			Log.d("MyCameraApp", "failed to create directory");
+		}
+		// Specify the file target for the photo
+		return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator +
+				fileName));
+	}
+
+	public void onLaunchCamera(View view) {
+		// create Intent to take a picture and return control to the calling application
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri("photo.jpg")); // set the image file name
+		// start the image capture Intent
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			Uri takenPhotoUri = getPhotoFileUri("photo.jpg");
+			ivShirtPic.setImageURI(takenPhotoUri);
+			// by this point we have the camera photo on disk
+			//Bitmap takenImage = BitmapFactory.decodeFile(filepath);
+		}
 	}
 	
 	/** Called when a user chooses the 'save' button. */
